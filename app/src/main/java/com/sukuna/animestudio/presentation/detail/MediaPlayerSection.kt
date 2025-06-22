f77a676907995629ae11c2069db75d506d05167b
devil-my1 <75006012+devil-my1@users.noreply.github.com>
2025-06-22 12:59:51 +0900

    fix invalid import causing build failure

--- app/src/main/java/com/sukuna/animestudio/presentation/detail/MediaPlayerSection.kt
+++ app/src/main/java/com/sukuna/animestudio/presentation/detail/MediaPlayerSection.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.ui.input.pointer.consume
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
